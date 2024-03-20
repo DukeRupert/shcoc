7a106b14f70200adcf705d32df092562dd26da57
Logan Williams <logan@fireflysoftware.dev>
2024-03-20 00:06:12 +0000

    Completed mobile menu on navbar component. Social links updated on footer component.

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,14 +1,25 @@
 <script lang="ts">
-	import { Button } from '$lib/components/ui/button';
-	import Lightswitch from './lightswitch.svelte';
 	import { SITE_DATA } from '$lib/global';
+	import { navigating } from '$app/stores';
+	import { MenuIcon } from 'lucide-svelte';
+	import { Separator } from '$lib/components/ui/separator/index.js';
+	import { Button } from '$lib/components/ui/button';
+	import * as Sheet from '$lib/components/ui/sheet';
+	import Lightswitch from './lightswitch.svelte';
+
+	let is_mobile_open = false;
+
+	// Reactive functions
+	$: if ($navigating) {
+		is_mobile_open = false; // Close mobile menu when navigation
+	}
 </script>
 
 <header class="absolute inset-x-0 top-0 z-50">
 	<nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
 		<div class="flex lg:flex-1">
-			<a href="#" class="-m-1.5 p-1.5">
-				<span class="sr-only">Your Company</span>
+			<a href="/" class="-m-1.5 p-1.5">
+				<span class="sr-only">{SITE_DATA.name}</span>
 				<img
 					class="h-8 w-auto"
 					src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
@@ -16,100 +27,57 @@
 				/>
 			</a>
 		</div>
-		<div class="flex lg:hidden">
-			<button
-				type="button"
-				class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-			>
-				<span class="sr-only">Open main menu</span>
-				<svg
-					class="h-6 w-6"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-					aria-hidden="true"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-					/>
-				</svg>
-			</button>
-		</div>
+
 		<div class="hidden lg:flex lg:gap-x-12">
 			{#each SITE_DATA.routes as { id, url }}
 				<Button href={url} variant="link" class="capitalize">{id}</Button>
 			{/each}
 		</div>
-		<div class="hidden lg:flex lg:flex-1 lg:justify-end">
+		<div class="flex space-x-4 lg:flex lg:flex-1 lg:justify-end">
 			<Lightswitch />
+			<!-- Mobile menu, show/hide based on menu open state. -->
+			<div class="lg:hidden" role="dialog" aria-modal="true">
+				<Sheet.Root bind:open={is_mobile_open}>
+					<Sheet.Trigger asChild let:builder>
+						<Button builders={[builder]} size="icon" variant="outline">
+							<span class="sr-only">Open navigation menu</span>
+							<MenuIcon class="h-6 w-6" /></Button
+						>
+					</Sheet.Trigger>
+					<Sheet.Content side="right">
+						<Sheet.Header>
+							<Sheet.Title>Navigation</Sheet.Title>
+							<Sheet.Description>
+								<q>
+									Ask and it’s given to you, search and you’ll find, knock and the door will open. ~ Matthew 7:7
+								</q>
+							</Sheet.Description>
+						</Sheet.Header>
+						<div class="mt-6">
+							<Separator class="-my-6" />
+							<div class="space-y-2 py-6">
+								<Button
+									href="/"
+									variant="link"
+									class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7"
+									>Home</Button
+								>
+								{#each SITE_DATA.routes as { id, url }}
+									<Button
+										href={url}
+										variant="link"
+										class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold capitalize leading-7"
+										>{id}</Button
+									>
+								{/each}
+							</div>
+							<!-- <div class="py-6">
+									<Button variant="default" href={SITE_DATA.online_order_url}>Order online</Button>
+								</div> -->
+						</div>
+					</Sheet.Content>
+				</Sheet.Root>
+			</div>
 		</div>
 	</nav>
-	<!-- Mobile menu, show/hide based on menu open state. -->
-	<div class="lg:hidden" role="dialog" aria-modal="true">
-		<!-- Background backdrop, show/hide based on slide-over state. -->
-		<div class="fixed inset-0 z-50"></div>
-		<div
-			class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
-		>
-			<div class="flex items-center justify-between">
-				<a href="#" class="-m-1.5 p-1.5">
-					<span class="sr-only">Your Company</span>
-					<img
-						class="h-8 w-auto"
-						src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-						alt=""
-					/>
-				</a>
-				<button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
-					<span class="sr-only">Close menu</span>
-					<svg
-						class="h-6 w-6"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						aria-hidden="true"
-					>
-						<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-					</svg>
-				</button>
-			</div>
-			<div class="mt-6 flow-root">
-				<div class="-my-6 divide-y divide-gray-500/10">
-					<div class="space-y-2 py-6">
-						<a
-							href="#"
-							class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-							>Product</a
-						>
-						<a
-							href="#"
-							class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-							>Features</a
-						>
-						<a
-							href="#"
-							class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-							>Marketplace</a
-						>
-						<a
-							href="#"
-							class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-							>Company</a
-						>
-					</div>
-					<div class="py-6">
-						<a
-							href="#"
-							class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-							>Log in</a
-						>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
 </header>
